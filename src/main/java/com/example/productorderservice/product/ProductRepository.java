d61231d8f82b8a448b75696278a7398335e2b778
선진 <s22013@gsm.hs.kr>
2023-08-04 23:26:51 +0900

    spring boot test

--- src/main/java/com/example/productorderservice/product/ProductRepository.java
+++ src/main/java/com/example/productorderservice/product/ProductRepository.java
@@ -1,8 +1,12 @@
 package com.example.productorderservice.product;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Repository
 class ProductRepository {
     private Map<Long, Product> persistence = new HashMap<>();
     private Long sequence = 0L;
