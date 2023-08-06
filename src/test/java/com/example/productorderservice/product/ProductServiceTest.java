6c123520cdf7ded786cf676380b25dde2e666a2d
선진 <s22013@gsm.hs.kr>
2023-08-06 23:54:36 +0900

    ApiTest

--- src/test/java/com/example/productorderservice/product/ProductServiceTest.java
+++ src/test/java/com/example/productorderservice/product/ProductServiceTest.java
@@ -1,12 +1,14 @@
 package com.example.productorderservice.product;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 
 @SpringBootTest
-class ProductServiceTest {
+class ProductApiTest {
 
     @Autowired
     private ProductService productService;
@@ -17,6 +19,13 @@ class ProductServiceTest {
         final AddProductRequest request = 상품등록요청_생성();
         productService.addProduct(request);
         // API 요청
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/products")
+                .then()
+                .log().all().extract();
     }
 
     private static AddProductRequest 상품등록요청_생성() {
